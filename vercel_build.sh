06a2579fcc84077575ba0b0ddd2148d356cc5604
Lucas-RM <lucas.marcondes36@gmail.com>
2025-11-16 15:57:20 -0300

    chore: update vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Atualiza o pip e instala as dependências
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Instala o pacote em modo produção
-python -m pip install .
+pip install .
 
 # Cria diretórios necessários
 mkdir -p __pycache__
